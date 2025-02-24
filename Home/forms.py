5d2679bd51f79202541b32d24594db08d2d816a9
SaiDhinakar <saidhinakars899@gmail.com>
2025-02-24 21:51:57 +0530

    Authentication completed. Working on Home page

---
+++ Home/forms.py
@@ -0,0 +1,26 @@
+# home/forms.py
+from django import forms
+from .models import Post, Comment
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'featured_image', 'categories', 'is_published']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
+            'categories': forms.SelectMultiple(attrs={'class': 'form-control'}),
+            'is_published': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={
+                'class': 'form-control', 
+                'rows': 3, 
+                'placeholder': 'Write your comment here...'
+            })
+        }
